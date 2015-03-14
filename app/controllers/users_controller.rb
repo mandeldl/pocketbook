81086888b13ae080e4141bd11ffd2cca92d1f86d
David Mandel <MandelDL@gmail.com>
2015-03-14 14:24:20 -0400

    authenticate sessions for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
 		#Otherwise redirect to '/users/new'.
 		@user = User.new(user_params)
     		if @user.save
-    			#session[:user_id] = @user.id
       			redirect_to('/about')
     		else
       			redirect_to('/users/new')
