74296516d4af0726417e7cf86c1b84b506eaa437
David Mandel <MandelDL@gmail.com>
2015-03-14 15:08:20 -0400

    add logout button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+	#In the Pages controller, use before_action to require 
+	#users to be logged in, except on the contact and help actions.
+
+	before_action :require_user, except: [:contact, :help]
+
 	def contact
 	end
 
