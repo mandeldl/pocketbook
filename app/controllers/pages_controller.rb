1afa7ea41f1d274ec557e3fa1b8864e87546e57d
David Mandel <MandelDL@gmail.com>
2015-03-14 12:52:22 -0400

    add pages for contact about and help

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
 	def signup
 	end
+
+	def contact
+	end
+
+	def help
+	end
+
+	def about
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get '/contact' => 'pages#contact'
+  get '/help' => 'pages#help'
+  get '/about' => 'pages#about'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
