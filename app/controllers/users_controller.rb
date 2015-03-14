3493161e6bd2c87dd02e5e0b4d733a79ba993fe6
David Mandel <MandelDL@gmail.com>
2015-03-14 13:55:19 -0400

    add create method with save and redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
 	def new
 		@user = User.create
 	end
+
+	def create
+		#In the Users controller, make an action named create to handle the POST requests. 
+		#Modify the code so that if user.save is successful, redirect to '/about'. 
+		#Otherwise redirect to '/users/new'.
+		@user = User.new(user_params)
+    		if @user.save
+    			#session[:user_id] = @user.id
+      			redirect_to('/about')
+    		else
+      			redirect_to('/users/new')
+    		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password)
+	end
 end
